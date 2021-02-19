23cd0681d21c23c2f1eccdbf706d9be776d04264
dazhi-wang <1527342335@qq.com>
2021-02-19 22:12:08 +0800

    clock更新

--- src/views/attendance/clock/index.vue
+++ src/views/attendance/clock/index.vue
@@ -1,145 +1,228 @@
 <template>
-  <div id="app">
-    <div class="clock">
-      <span class="text">
-        {{ year }}年 {{ month }} 月 {{ date1 }} 日</br>
-        星期{{ day }}
-        {{ time }}
-      </span>
-      <!-- <button class="btn btn-clock" @click="selectClock" :class="{'to-left': index != 0}">时钟</button> -->
-      <!-- <button class="btn btn-timer" @click="selectTimer" v-if="index == 0">
-        <span>计时器</span>
-      </button> -->
-      <!-- <button class="btn btn-suspend" @click="suspendTimer" v-if="index > 0">
-        <span v-if="index == 1">暂停</span>
-        <span v-if="index == 2">开始</span>
-      </button>
-      <button class="btn btn-reset" @click="resetTimer" v-if="index == 1 || index == 2">
-        <span>重置</span>
-      </button> -->
+  <div id="clock">
+    <div class="c-up">
+      <div :class="clockClass">
+        <p class="text">
+          {{ year }}年 {{ month }} 月 {{ date }} 日</br>
+          星期{{ day }}
+          {{ time }}
+        </p>
+        <el-button type="text" v-show="workInbut" @click="clockIn">上班打卡</el-button>
+        <el-button type="text" v-show="workOutbut" @click="clockOut">下班打卡</el-button>
+      </div>
     </div>
-    <!-- <location></location> -->
+    
+    <el-divider content-position="center">今日打卡</el-divider>
+    <div class="c-list">
+      <div class="l-left">
+        <el-table :data="tableData" :row-class-name="tableRowClassName" style="width: 100%">
+          <el-table-column prop="date" label="日期" width="180">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="180">
+          </el-table-column>
+          <el-table-column prop="time" label="打卡时间">
+          </el-table-column>
+          <el-table-column prop="state" label="打卡状态" width="100">
+          </el-table-column>
+          <el-table-column prop="type" label="打卡类型" width="100">
+          </el-table-column>
+        </el-table>
+      </div>
+      <div class="l-right">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// import AMap from 'AMap'
-// import location from './location/index'
-var icnow = new Date();
-const days = ['天', '一', '二', '三', '四', '五', '六']; // 星期数组
-export default {
-  components: {
-    // location
-  },
-  data() {
-    return {
-      year: icnow.getFullYear(),
-      month: icnow.getMonth() + 1,
-      date1: icnow.getDate(),
-      day: days[icnow.getDay()],
-      time: icnow.toTimeString().substring(0, 8),
-      index: 0,   // 0表示时钟页面，1表示计时器计时状态，2表示计时器暂停状态
-      timer: null,
-      date: null
-    }
-  },
-  watch: {
-    // index(newValue, oldValue) {
-    //   clearInterval(this.timer);
-    //   this.timer = null;
-    //   this.date = null;
-    //   // 从时钟页面click过来 或 从计时器页面click过来
-    //   if (oldValue == 0 || newValue == 0) {   // index等于2时数据保留
-    //     // this.hour = '00';
-    //     // this.min = '00';
-    //     // this.sec = '00';
-    //     // this.h = 0;
-    //     // this.m = 0;
-    //     // this.s = 0;
-    //   }
-    //   this.autoMove();
-    // }
-  },
-  created() {
-    //   this.init()
-  },
-  methods: {
-    // 自动计时
-    autoMove() {
-      this.timer = setInterval(() => {
-        this.date = new Date();
-        this.year = this.date.getFullYear();
-        this.month = this.date.getMonth() + 1;
-        this.date1 = this.date.getDate();
-        this.day = days[this.date.getDay()];
-        this.time = this.date.toTimeString().substring(0, 8);
-      }, 1000);
+  var icnow = new Date();
+  const days = ['天', '一', '二', '三', '四', '五', '六']; // 星期数组
+  export default {
+    data() {
+      return {
+        clockClass: 'c-clock-green',
+        workOutbut: '',
+        workInbut: '',
+        workInlog: false,
+        workOutlog: false,
+        year: icnow.getFullYear(),
+        month: icnow.getMonth() + 1,
+        date: icnow.getDate(),
+        day: days[icnow.getDay()],
+        time: icnow.toTimeString().substring(0, 8),
+        timer: null,
+        tableData: [
+          {
+            date: '2016-05-04',
+            name: '王小虎1',
+            time: '10:00:23',
+            state: '迟到',
+            type: '上班'
+          },
+          {
+            date: '2016-05-03',
+            name: '王小虎2',
+            time: '19:00:23',
+            state: '正常',
+            type: '下班'
+          }]
+      }
     },
-    // init(){
-    //     this.date = new Date();
-    //     this.h = this.date.getHours();
-    //     this.m = this.date.getMinutes();
-    //     this.s = this.date.getSeconds();
-    //     this.hour = this.h > 9 ? this.h : '0' + this.h;
-    //     this.min = this.m > 9 ? this.m : '0' + this.m;
-    //     this.sec = this.s > 9 ? this.s : '0' + this.s;
-    // }
-  },
-  mounted() {
-    this.autoMove();
+    watch: {
+
+    },
+    created() {
+      this.init1()
+      this.init()
+    },
+    methods: {
+      init1(){
+        this.workOutbut = false
+        this.workInbut = true
+      },
+      init() {
+        // this.workOutbut = false
+        // this.workInbut = true
+        let day = icnow.getDay()
+        let time = icnow.getHours()
+        if (day != 0 && day != 6) { //工作日
+          if (time < 8) { //8点之前
+            if (this.workInbut == true) {
+              this.clockClass = 'c-clock-green'
+            } else if (this.workOutbut == true) {
+              this.clockClass = 'c-clock-red'
+            }
+          } else if (time >= 8 && time < 18) { //8点到17点之间
+            if (this.workInbut == true) {
+              this.clockClass = 'c-clock-red'
+            } else if (this.workOutbut == true && this.workOutlog == true) {
+              this.clockClass = 'c-clock-red'
+            } else if (this.workOutbut == true && this.workOutlog == false) {
+              this.clockClass = 'c-clock-green'
+            }
+          } else if (time > 17) {//17点之后
+            if (this.workInbut == true && this.workInlog == false) {
+              this.clockClass = 'c-clock-red'
+            } else if (this.workInbut == true && this.workInlog == true) {
+              this.clockClass = 'c-clock-green'
+            } else if (this.workOutbut == true && this.workOutlog == true) {
+              this.clockClass = 'c-clock-green'
+            } else if (this.workOutbut == true && this.workOutlog == false) {
+              this.clockClass = 'c-clock-green'
+            }
+          }
+        } else { //周六周日
+          if (this.workInbut == true && this.workInlog == false) {
+            this.clockClass = 'c-clock-green'
+          } else if (this.workInbut == true && this.workInlog == true) {
+            this.clockClass = 'c-clock-green'
+          } else if (this.workOutbut == true && this.workOutlog == true) {
+            this.clockClass = 'c-clock-green'
+          } else if (this.workOutbut == true && this.workOutlog == false) {
+            this.clockClass = 'c-clock-green'
+          }
+        }
+      },
+      clockIn() {
+        this.workInlog = true
+        this.workOutbut = true
+        this.workInbut = false
+        this.init()
+      },
+      clockOut() {
+        this.workOutlog = true
+        // this.workOutbut = true
+        // this.workInbut = false
+        this.init()
+      },
+      //记录背景色
+      tableRowClassName({ row, rowIndex }) {
+        if (row.state === "正常") {
+          return 'success-row';
+        } else {
+          return 'warning-row';
+        }
+        return '';
+      },
+      // 自动计时
+      autoMove() {
+        this.timer = setInterval(() => {
+          this.date = new Date();
+          this.year = icnow.getFullYear();
+          this.month = icnow.getMonth() + 1;
+          this.date = icnow.getDate();
+          this.day = days[icnow.getDay()];
+          this.time = icnow.toTimeString().substring(0, 8);
+        }, 1000);
+      },
+    },
+    mounted() {
+      this.autoMove();
+    }
   }
-}
 </script>
 
+<style>
+  .el-table .warning-row {
+    background: oldlace;
+  }
 
+  .el-table .success-row {
+    background: #f0f9eb;
+  }
+</style>
 <style scope>
-.clock {
-  width: 400px;
-  height: 180px;
-  line-height: 90px;
-  border: 10px solid #aaa;
-  border-radius: 10px;
-  margin: 120px auto;
-  background: pink;
-  text-align: center;
-  position: relative;
-  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.6);
-}
-.clock .text {
-  font-size: 20px;
-  font-weight: bold;
-  color: rgba(0, 0, 0, 0.7);
-}
-.clock .btn {
-  position: absolute;
-  /*top: -66px;*/
-  bottom: -66px;
-  border: none;
-  outline: none;
-  width: 80px;
-  height: 36px;
-  border-radius: 4px;
-  font-size: 16px;
-  background: #aaa;
-  cursor: pointer;
-  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.6);
-}
-.clock .btn:hover {
-  opacity: 0.8;
-}
-.clock .btn-clock {
-  left: 110px;
-}
-.clock .btn-clock.to-left {
-  left: 60px;
-}
-.clock .btn-timer {
-  right: 110px;
-}
-.clock .btn-suspend {
-  right: 160px;
-}
-.clock .btn-reset {
-  right: 60px;
-}
+  .c-up{
+    display: flex;
+  }
+  .c-clock-green {
+    width: 300px;
+    height: 300px;
+    line-height: 50px;
+    border-radius: 100%;
+    margin: 40px auto;
+    background: rgb(165, 255, 75);
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    box-shadow: 0px 5px 20px rgb(124, 252, 0);
+  }
+
+  .c-clock-red {
+    width: 300px;
+    height: 300px;
+    line-height: 50px;
+    border-radius: 100%;
+    margin: 40px auto;
+    background: rgb(255, 75, 75);
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    box-shadow: 0px 5px 20px rgb(255, 75, 75);
+  }
+
+  .c-clock .text {
+    height: 100px;
+    font-size: 20px;
+    font-weight: bold;
+    color: rgba(0, 0, 0, 0.7);
+  }
+
+  .c-list {
+    display: flex;
+
+  }
+
+  .l-left {
+    width: 70%;
+  }
+
+  .l-right {
+    width: 30%;
+  }
 </style>
